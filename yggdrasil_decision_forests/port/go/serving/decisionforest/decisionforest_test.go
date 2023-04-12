6fe3cf177aba05a8cfa0e42c29c0b3005bbd156a
Arnold1 <surfman19@gmx.at>
2023-04-11 22:30:46 -0400

    Update decisionforest_test.go

--- yggdrasil_decision_forests/port/go/serving/decisionforest/decisionforest_test.go
+++ yggdrasil_decision_forests/port/go/serving/decisionforest/decisionforest_test.go
@@ -22,7 +22,7 @@ package decisionforest
 import (
 	"testing"
 
-	"github.com/Arnold1/yggdrasil-decision-forests-1/yggdrasil_decision_forests/port/go/utils/test"
+	"github.com/google/yggdrasil-decision-forests/yggdrasil_decision_forests/port/go/utils/test"
 )
 
 func TestGetSetBit(t *testing.T) {
